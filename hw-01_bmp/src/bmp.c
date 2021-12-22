c07232914577b833b0ab501ec52e1f1e60f0445f
seva-vaskin <vaskin.2003@gmail.com>
2021-12-23 01:29:25 +0300

    implement basic task

---
+++ hw-01_bmp/src/bmp.c
@@ -0,0 +1,85 @@
+#include "bmp.h"
+
+LONG get_padding_size(LONG row_length) {
+    return (PADDING_ALIGNMENT - row_length % PADDING_ALIGNMENT) % PADDING_ALIGNMENT;
+}
+
+LONG get_row_length_with_padding(LONG row_length) {
+    return row_length + get_padding_size(row_length);
+}
+
+void load_bmp(BMP *bmp, FILE *stream) {
+    // header reading
+    fread(&bmp->bit_map_file_header, sizeof(BIT_MAP_FILE_HEADER), 1, stream);
+    fread(&bmp->bit_map_info, sizeof(BIT_MAP_INFO), 1, stream);
+
+    // data reading
+    bmp->flat_data = (PIXEL*)malloc(bmp->bit_map_info.bi_size_image * sizeof(PIXEL));
+    fread(bmp->flat_data, bmp->bit_map_info.bi_size_image, 1, stream);
+    init_data(bmp);
+}
+
+void init_data(BMP *bmp) {
+    bmp->data = (PIXEL**)malloc(bmp->bit_map_info.bi_height * sizeof (PIXEL *));
+    LONG row_length_with_padding = get_row_length_with_padding(bmp->bit_map_info.bi_width);
+    for (int i = 0; i < bmp->bit_map_info.bi_height; i++) {
+        bmp->data[i] = &bmp->flat_data[i * row_length_with_padding];
+    }
+}
+
+void free_bmp(BMP *bmp) {
+    if (!bmp)
+        return;
+    free(bmp->flat_data);
+    free(bmp->data);
+    free(bmp);
+}
+
+BMP* create_bmp(BMP* header_src, int w, int h) {
+    BMP*result = (BMP*)malloc(sizeof(BMP));
+    *result = *header_src;
+    DWORD image_size = sizeof(PIXEL) * h * get_row_length_with_padding(w);
+    result->bit_map_file_header.bf_size = image_size + sizeof(BIT_MAP_FILE_HEADER) + sizeof(BIT_MAP_INFO);
+    result->bit_map_info.bi_height = h;
+    result->bit_map_info.bi_width = w;
+    result->bit_map_info.bi_size_image = image_size;
+    result->flat_data = (PIXEL*)calloc(image_size, 1);
+    init_data(result);
+    return result;
+}
+
+void save_bmp(BMP *bmp, FILE *stream) {
+    fwrite(&bmp->bit_map_file_header, sizeof(BIT_MAP_FILE_HEADER), 1, stream);
+    fwrite(&bmp->bit_map_info, sizeof(BIT_MAP_INFO), 1, stream);
+    fwrite(bmp->flat_data, sizeof(PIXEL), bmp->bit_map_info.bi_size_image, stream);
+}
+
+bool check_bmp_subrectangular(BMP *bmp, LONG x, LONG y, LONG w, LONG h) {
+    return 0 <= x && 0 <= y && x + w <= bmp->bit_map_info.bi_width && y + h <= bmp->bit_map_info.bi_height;
+}
+
+BMP* crop(BMP *bmp, LONG x, LONG y, LONG w, LONG h) {
+    if (!check_bmp_subrectangular(bmp, x, y, w, h))
+        return NULL;
+    BMP *result = create_bmp(bmp, w, h);
+    for (LONG i = 0; i < h; i++) {
+        for (LONG j = 0; j < w; j++) {
+            result->data[i][j] = bmp->data[x + i][y + j];
+        }
+    }
+    return result;
+}
+
+BMP* rotate(BMP *bmp) {
+    LONG w = bmp->bit_map_info.bi_width;
+    LONG h = bmp->bit_map_info.bi_height;
+    BMP *result = create_bmp(bmp, h, w);
+    for (LONG i = 0; i < h; i++) {
+        for (LONG j = 0; j < w; j++) {
+            result->data[w - j - 1][i] = bmp->data[i][j];
+        }
+    }
+    return result;
+}
+
+
