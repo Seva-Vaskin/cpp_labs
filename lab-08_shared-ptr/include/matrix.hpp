9a100c3ec4bd2a742493f8b99b7ab57a26eda3b7
seva-vaskin <vaskin.2003@gmail.com>
2022-03-10 00:22:21 +0300

    done

--- lab-08_shared-ptr/include/matrix.hpp
+++ lab-08_shared-ptr/include/matrix.hpp
@@ -1,26 +1,52 @@
 #pragma once
 
+#include <cstdio>
 #include <cstddef>
+#include <cstring>
+#include <cassert>
+#include <algorithm>
 
 class Matrix {
 public:
     Matrix(std::size_t r, std::size_t c);
+
+    Matrix(const Matrix &m);
+
     ~Matrix();
 
     std::size_t get_rows() const;
+
     std::size_t get_cols() const;
+
     void set(std::size_t i, std::size_t j, int val);
+
     int get(std::size_t i, std::size_t j) const;
 
-    bool operator==(const Matrix& that) const;
-    bool operator!=(const Matrix& that) const;
+    void print(FILE *f) const;
 
-private:
-    Matrix(const Matrix& that);
-    Matrix& operator=(const Matrix& that);
+    Matrix operator+(const Matrix &m) const;
+
+    Matrix operator-(const Matrix &m) const;
+
+    Matrix operator*(const Matrix &m) const;
+
+    Matrix &operator+=(const Matrix &m);
+
+    Matrix &operator-=(const Matrix &m);
+
+    Matrix &operator*=(const Matrix &m);
+
+    Matrix &operator=(const Matrix &m);
+
+    bool operator==(const Matrix &m) const;
+
+    bool operator!=(const Matrix &m) const;
 
 private:
     std::size_t _rows;
     std::size_t _cols;
     int **_data;
+
+    void swap(Matrix &m);
 };
+
