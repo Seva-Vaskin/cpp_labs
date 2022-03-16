db6708373aeaadb66666460c53412c6470c1ae6e
seva-vaskin <vaskin.2003@gmail.com>
2022-03-16 15:02:02 +0300

    done

--- lab-09_figures/include/Rectangle.hpp
+++ lab-09_figures/include/Rectangle.hpp
@@ -1,8 +1,22 @@
+#pragma once
+
 #include "Figure.hpp"
+#include <cstdio>
+#include <cmath>
 
 class Rectangle : public Figure {
 public:
     Rectangle(int id, int x, int y, int width, int height);
 
-    // ...
+    ~Rectangle() override;
+
+    void print() const override;
+
+    bool is_inside(int x, int y) const override;
+
+    void zoom(int factor) override;
+
+protected:
+    int width;
+    int height;
 };
--- lab-09_figures/src/Circle.cpp
+++ lab-09_figures/src/Circle.cpp
@@ -1,3 +1,26 @@
 #include "Circle.hpp"
 
-// TODO
+Circle::Circle(int id, int x, int y, int radius, const char *label) : Figure(id, x, y), radius(radius) {
+    int label_length = std::strlen(label);
+    char * tmp = new char[label_length + 1];
+    std::strcpy(tmp, label);
+    this->label = tmp;
+}
+
+Circle::~Circle() {
+    delete[] label;
+}
+
+void Circle::print() const {
+    printf("Circle %d: x = %d y = %d radius = %d label = %s\n", id, x, y, radius, label);
+}
+
+bool Circle::is_inside(int x, int y) const {
+    return (x - this->x) * (x - this->x) + (y - this->y) * (y - this->y) <= radius * radius;
+}
+
+void Circle::zoom(int factor) {
+    radius *= factor;
+}
+
+
