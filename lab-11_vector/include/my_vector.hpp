381ef7b62789f7cb51ea40858825f89e4f918559
seva-vaskin <vaskin.2003@gmail.com>
2022-04-03 01:09:07 +0300

    finish task

--- lab-11_vector/include/my_vector.hpp
+++ lab-11_vector/include/my_vector.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cstddef>
+#include <cstring>
 
 namespace containers {
 
@@ -10,45 +11,127 @@ namespace containers {
         my_vector() {
             capacity_ = 2;
             size_ = 0;
-            array_ = new char[sizeof(T) * capacity_];
+            array_ = reinterpret_cast<T *>(new char[sizeof(T) * capacity_]);
         }
 
         explicit my_vector(std::size_t n) : my_vector() {
             static_assert(std::is_default_constructible<T>::value);
             reserve(n);
             size_ = n;
-            array_ = new T[n];
+            array_ = new T[n]();
         }
 
-        explicit my_vector(std::size_t n, const T& default_value) : my_vector() {
-
+        explicit my_vector(std::size_t n, const T &default_value) : my_vector() {
+            static_assert(std::is_copy_constructible<T>::value);
+            reserve(n);
+            size_ = n;
+            for (size_t i = 0; i < n; i++) {
+                new(array_ + i) T(default_value);
+            }
         }
 
-        my_vector(my_vector other);
+        my_vector(const my_vector &other) {
+            static_assert(std::is_copy_constructible<T>::value);
+            capacity_ = other.capacity_;
+            size_ = other.size_;
+            array_ = reinterpret_cast<T *>(new char[sizeof(T) * capacity_]);
+            for (size_t i = 0; i < size_; i++) {
+                new (array_ + i) T(other[i]);
+            }
+        }
 
-        my_vector operator=(my_vector other);
+        my_vector &operator=(const my_vector &other) {
+            if (this == &other)
+                return *this;
+            my_vector<T> tmp = other;
+            swap(tmp);
+            return *this;
+        }
 
-        ~my_vector();
+        ~my_vector() {
+            for (size_t i = 0; i < size_; i++) {
+                array_[i].~T();
+            }
+            delete[] reinterpret_cast<char *>(array_);
+        }
 
-        std::size_t size();
+        std::size_t size() const {
+            return size_;
+        }
 
-        std::size_t capacity();
+        std::size_t capacity() const {
+            return capacity_;
+        }
 
-        bool empty();
+        bool empty() const {
+            return size_ == 0;
+        }
 
-        void resize(std::size_t n);
+        void resize(std::size_t n) {
+            static_assert(std::is_default_constructible<T>::value);
+            if (n <= size_) {
+                size_ = n;
+                return;
+            }
+            reserve(n);
+            for (size_t i = size_; i < n; i++)
+                array_[i] = T();
+            size_ = n;
+        }
 
-        void reserve(std::size_t n);
+        void reserve(std::size_t n) {
+            if (n < capacity_)
+                return;
+            size_t new_capacity = capacity_;
+            while (new_capacity < n)
+                new_capacity <<= 1;
+            char *new_array = new char[sizeof(T) * new_capacity];
+            std::memcpy(new_array, array_, sizeof(T) * size_);
+            delete[] reinterpret_cast<char *>(array_);
+            array_ = reinterpret_cast<T *>(new_array);
+            capacity_ = new_capacity;
+        }
 
-        ??
+        T &operator[](std::size_t index) {
+            return array_[index];
+        }
 
-        operator[](std::size_t index);
+        const T &operator[](std::size_t index) const {
+            return array_[index];
+        }
 
-        void push_back(T t);
+        void push_back(const T &t) {
+            reserve(size_ + 1);
+            new(array_ + size_) T(t);
+            size_++;
+        }
 
-        void pop_back();
+        void pop_back() {
+            size_--;
+            array_[size_].~T();
+        }
 
-        void clear();
+        void clear() {
+            for (size_t i = 0; i < size_; i++)
+                array_[i].~T();
+            size_ = 0;
+        }
+
+        friend std::ostream &operator<<(std::ostream &out, const my_vector &my_vec) {
+            for (size_t i = 0; i < my_vec.size_; i++) {
+                out << my_vec[i];
+                if (i + 1 < my_vec.size_)
+                    out << ' ';
+            }
+            return out;
+        }
+
+    private:
+        void swap(my_vector &other) {
+            std::swap(capacity_, other.capacity_);
+            std::swap(size_, other.size_);
+            std::swap(array_, other.array_);
+        }
 
     private:
         size_t capacity_{};
