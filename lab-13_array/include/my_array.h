144df9c438b7b192f289932259dd041fa3e6f15d
seva-vaskin <vaskin.2003@gmail.com>
2022-05-02 17:21:53 +0300

    done

--- lab-13_array/include/my_array.h
+++ lab-13_array/include/my_array.h
@@ -1,22 +1,127 @@
-#ifndef MY_ARRAY_H_
-#define MY_ARRAY_H_
+#pragma once
 
 #include <cstddef>
+#include <stdexcept>
+#include <algorithm>
 
 namespace lab_13 {
 
-template<typename T, std::size_t N>
-class my_array {
-public:
-    at(std::size_t index);
-    operator[](std::size_t index);
+    template<typename T, size_t N>
+    class my_array {
+    private:
+        T _data[N];
+    public:
+        my_array() {
+            for (size_t i = 0; i < N; i++) {
+                new(_data + i) T();
+            }
+        }
 
-    bool empty();
-    size();
+        my_array(const my_array &other_array) {
+            for (size_t i = 0; i < N; i++)
+                _data[i] = other_array[i];
+        }
 
-    fill(T val);
-};
+        const T &at(size_t index) const {
+            if (index >= N)
+                throw std::out_of_range("Array index out of range");
+            return _data[index];
+        }
 
-}  // namespace lab_13
+        T &at(size_t index) {
+            if (index >= N)
+                throw std::out_of_range("Array index out of range");
+            return _data[index];
+        }
 
-#endif  // MY_ARRAY_H_
+        const T &operator[](size_t index) const {
+            return _data[index];
+        }
+
+        T &operator[](size_t index) {
+            return _data[index];
+        }
+
+        bool empty() const {
+            return N == 0;
+        }
+
+        size_t size() const {
+            return N;
+        }
+
+        void fill(const T &val) {
+            std::fill(_data, _data + N, val);
+        }
+    };
+
+    template<size_t N>
+    class my_array<bool, N> {
+    private:
+        uint8_t _data[(N + 7) / 8] = {};
+
+        class proxy {
+        private:
+            uint8_t &_storage;
+            uint8_t _pos;
+        public:
+            proxy(uint8_t &storage, uint8_t pos) : _storage(storage), _pos(pos) {}
+
+            operator bool() const {
+                return _storage >> _pos & 1;
+            }
+
+            proxy& operator=(bool new_value) {
+                _storage &= uint8_t(-1) ^ (1 << _pos);
+                _storage |= new_value << _pos;
+                return *this;
+            }
+            proxy& operator=(const proxy& other_proxy) {
+                return operator=(bool(other_proxy));
+            }
+        };
+
+    public:
+        my_array() = default;
+
+        my_array(const my_array &other_array) {
+            std::copy(other_array._data, other_array._data + (N + 7) / 8, _data);
+        }
+
+        bool operator[](size_t index) const {
+            return _data[index / 8] >> (index % 8) & 1;
+        }
+
+        proxy operator[](size_t index) {
+            return proxy(_data[index / 8], index % 8);
+        }
+
+        bool at(size_t index) const {
+            if (index >= N)
+                throw std::out_of_range("Array index out of range");
+            return operator[](index);
+        }
+
+        proxy at(size_t index) {
+            if (index >= N)
+                throw std::out_of_range("Array index out of range");
+            return operator[](index);
+        }
+
+        bool empty() const {
+            return N == 0;
+        }
+
+        bool size() const {
+            return N;
+        }
+
+        void fill(bool new_value) {
+            if (new_value)
+                std::fill(_data, _data + (N + 7) / 8, uint8_t(-1));
+            else
+                std::fill(_data, _data + (N + 7) / 8, uint8_t(0));
+        }
+    };
+}
+
