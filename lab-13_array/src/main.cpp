ae6fc5460f4cdfe53361c3bd09839be4566b2088
seva-vaskin <vaskin.2003@gmail.com>
2022-05-02 17:39:16 +0300

    delete local testing output

--- lab-13_array/src/main.cpp
+++ lab-13_array/src/main.cpp
@@ -1,26 +1,26 @@
-//#include "../src/smoke_compile.cpp"
+#include "../src/smoke_compile.cpp"
 
-#include <iostream>
-#include "my_array.h"
-
-using lab_13::my_array;
-using std::cout;
-using std::endl;
-
-int main() {
-    my_array<bool, 10> a;
-    cout << a.size() << ' ' << a.empty() << endl;
-    a.fill(false);
-    for (int i = 0; i < 10; i++)
-        cout << a[i] << ' ';
-    cout << endl;
-    a.fill(true);
-    for (int i = 0; i < 10; i++)
-        cout << a[i] << ' ';
-    cout << endl;
-    a[9] = false;
-    a[0] = a[1] = false;
-    for (int i = 0; i < 10; i++)
-        cout << a[i] << ' ';
-    cout << endl;
-}
+//#include <iostream>
+//#include "my_array.h"
+//
+//using lab_13::my_array;
+//using std::cout;
+//using std::endl;
+//
+//int main() {
+//    my_array<bool, 10> a;
+//    cout << a.size() << ' ' << a.empty() << endl;
+//    a.fill(false);
+//    for (int i = 0; i < 10; i++)
+//        cout << a[i] << ' ';
+//    cout << endl;
+//    a.fill(true);
+//    for (int i = 0; i < 10; i++)
+//        cout << a[i] << ' ';
+//    cout << endl;
+//    a[9] = false;
+//    a[0] = a[1] = false;
+//    for (int i = 0; i < 10; i++)
+//        cout << a[i] << ' ';
+//    cout << endl;
+//}
